a555d0062323d98c67263825d19cb482248db2ec
Mutsarano3 <thomas210574@outlook.be>
2021-08-09 09:08:24 +0200

    push test project

---
+++ Gestion_brasserieTest/BrewerTest.cs
@@ -0,0 +1,82 @@
+using Gestion_brasserie2021_CHIARELLI_THOMAS;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.Controllers;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.DataContext;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.IRepository;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.Models;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.Repository;
+using Gestion_brasserie2021_CHIARELLI_THOMAS.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace Gestion_brasserieTest
+{
+	[TestClass()]
+	public class BrewerTest
+	{
+		
+
+		
+
+
+
+
+
+		[TestMethod()]
+		public  void AddBeer()
+		{
+			Beer beer = new Beer();
+			beer.IdBrewer = 1;
+			beer.Name = "TestLeffe";
+			beer.Price = 12.2;
+			beer.Alchool = 5.2;
+
+			/*var mockContext  = new Mock<UnitOfWork>();
+			mockContext.Setup(m => m.Beers.SaveOrUpdate(beer)).Returns(beer);
+			var controller = new BrewerController(mockContext.Object);
+			*/
+			Assert.AreEqual("ok", "ok");
+			/*
+
+			Beer beer = new Beer();
+			beer.IdBrewer = 1;
+			beer.Name = "TestLeffe";
+			beer.Price = 12.2;
+			beer.Alchool = 5.2;
+		
+			
+			//Assert.AreEqual("ok", "ok");
+			
+			
+			if (beer == null)
+			{
+				
+			}
+
+			if (string.IsNullOrEmpty(beer.Name))
+			{
+				
+			}
+
+			_unitOfWork.Beers.SaveOrUpdate(beer);
+			_unitOfWork.Complete();
+			Assert.AreEqual("ok", "ok");
+			*/
+
+
+
+
+
+		}
+	}
+}
+
+
---
+++ Gestion_brasserieTest/Endpoints/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestion_brasserieTest.Endpoints
+{
+	public static class Endpoints
+	{
+		public static string BASE_URL = "https://localhost:5001/api";
+		#region BREWER_REGION
+		public static string NEW_BEER = BASE_URL + "/bewer/newbeer";
+		#endregion
+	}
+}
